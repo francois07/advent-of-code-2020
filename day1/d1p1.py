431bb49f5c2766db28d18a891d63d523b67fbc91
francois07 <francois.soulie@edu.esiee.fr>
2020-12-01 21:15:16 +0100

    day1

---
+++ day1/d1p1.py
@@ -0,0 +1,11 @@
+input = [int(x) for x in open('input.txt', 'r').read().splitlines()]
+
+def find_pair(data, sum=2020):
+    res=-1
+    for i in input:
+        for j in input:
+            if i+j==2020:
+                res=i*j
+    return res
+
+print(find_pair(input))
---
+++ day1/d1p2.py
@@ -0,0 +1,12 @@
+input = [int(x) for x in open('input.txt', 'r').read().splitlines()]
+
+def find_triplet(data, sum=2020):
+    res=-1
+    for i in input:
+        for j in input:
+            for h in input:
+                if i+j+h==2020:
+                    res=i*j*h
+    return res
+
+print(find_triplet(input))
