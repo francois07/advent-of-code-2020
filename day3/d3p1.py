489992d68a313accbebfd1b4669b6c7f1aeef6f0
francois07 <francois.soulie@edu.esiee.fr>
2020-12-04 10:10:00 +0100

    changed return type of check_position to boolean & better conditions

--- day3/d3p1.py
+++ day3/d3p1.py
@@ -2,10 +2,7 @@ input = [x for x in open('input.txt', 'r').read().splitlines()]
 
 
 def check_position(data, x, y):
-    if data[y][x % len(data[0])] == '#':
-        return 1
-    else:
-        return 0
+    return data[y][x % len(data[0])] == '#'
 
 
 def check_slope(data, slope):
@@ -14,7 +11,8 @@ def check_slope(data, slope):
     for i in range(height):
         if slope[1]*i > height:
             break
-        n += check_position(data, slope[0]*i, slope[1]*i)
+        if check_position(data, slope[0]*i, slope[1]*i):
+            n += 1
     return n
 
 
