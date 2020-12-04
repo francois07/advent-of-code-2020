df425201394fe9b1e734476f48f029a72dd4a4f5
francois07 <francois.soulie@edu.esiee.fr>
2020-12-04 19:58:51 +0100

    better check functions & check_passport conditions

--- day4/d4p2.py
+++ day4/d4p2.py
@@ -6,50 +6,33 @@ FIELDS = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid']
 COLORS = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
 
 
-def check_digits(data, n):
-    if len(data) != n:
-        return False
-    else:
-        return True
-
-
-def check_between(data, x, y):
-    n = int(data)
-    if n < x or n > y:
-        return False
-    else:
-        return True
-
-
 def check_year(year, x, y):
-    return check_digits(year, 4) and check_between(year, x, y)
+    return len(year) == 4 and (x <= int(year) <= y)
 
 
 def check_height(height, x_cm, y_cm, x_in, y_in):
-    u = height[-2:]
-    n = height[:-2]
-    if u == 'cm':
-        return check_between(n, x_cm, y_cm)
-    if u == 'in':
-        return check_between(n, x_in, y_in)
+    unit = height[-2:]
+    measure = height[:-2]
+    if unit == 'cm':
+        return (x_cm <= int(measure) <= y_cm)
+    elif unit == 'in':
+        return (x_in <= int(measure) <= y_in)
+    else:
+        return False
 
 
 def check_hex(hexa):
-    h = hexa[1:]
-    if hexa[0] != '#' or not check_digits(hexa, 7):
-        print(1)
+    value = hexa[1:]
+    if hexa[0] != '#' or len(hexa) != 7:
         return False
-    for l in h:
-        if not 0 <= int(l, base=16) < 16:
+    for char in value:
+        if not (0 <= int(char, base=16) < 16):
             return False
     return True
 
 
-def check_color(data, colors=COLORS):
-    if data not in colors:
-        return False
-    else:
-        return True
+def check_color(color, colors=COLORS):
+    return color in colors
 
 
 def check_rules(field, data):
@@ -66,7 +49,9 @@ def check_rules(field, data):
     elif field == "ecl":
         return check_color(data)
     elif field == "pid":
-        return check_digits(data, 9)
+        return len(data) == 9 and data.isnumeric()
+    else:
+        return False
 
 
 def parse_fields(data):
@@ -79,18 +64,17 @@ def parse_fields(data):
 def check_passport(data, fields=FIELDS, optional=[]):
     passport = parse_fields(data)
     for field in fields:
-        if field not in optional and field not in passport:
-            return False
-        elif field in passport and field not in optional:
-            if not check_rules(field, passport[field]):
+        if field not in optional:
+            if field not in passport:
                 return False
+            else:
+                if not check_rules(field, passport[field]):
+                    return False
     return True
 
 
-n = 0
+i = 0
 for passport in input:
     if check_passport(passport, optional=["cid"]):
-        n += 1
-    else:
-        print(passport+"\n---")
-print(n)
+        i += 1
+print(i)
