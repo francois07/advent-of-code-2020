fc438952ace786acd471e66f0f4198e0151620b1
francois07 <francois.soulie@edu.esiee.fr>
2020-12-07 19:07:51 +0100

    day7

---
+++ day7/d7p1.py
@@ -0,0 +1,38 @@
+import re
+
+input = [x for x in open('input.txt', 'r').read().splitlines()]
+
+
+def parse_rules(data):
+    res = {}
+    for rule in data:
+        split = re.split(r' bags contain | bags, | bag,', rule)
+        bag_color = split[0]
+        contained_bags = []
+        for contained in split[1:]:
+            stripped = re.sub(r' bags\.| bag\.', '', contained)
+            count = re.search(r'\d+', stripped)
+            if count:
+                color = re.sub(r'\d+', '', stripped).strip()
+                contained_bags.append((count.group(), color))
+        res[bag_color] = contained_bags
+    return res
+
+
+def find_containers(color, parsed_data):
+    match = [x for x in parsed_data if len(
+        [y for y in parsed_data[x] if y[1] == color]) >= 1]
+    return match
+
+
+def count_containers_recursive(color, parsed_data, x=[]):
+    containers = find_containers(color, parsed_data)
+    n = len(containers) - len([y for y in containers if y in x])
+    [x.append(y) for y in containers]
+    for color in containers:
+        n += count_containers_recursive(color, parsed_data, x)
+    return n
+
+
+parsed_rules = parse_rules(input)
+print(count_containers_recursive("shiny gold", parsed_rules))
---
+++ day7/d7p2.py
@@ -0,0 +1,30 @@
+import re
+
+input = [x for x in open('input.txt', 'r').read().splitlines()]
+
+
+def parse_rules(data):
+    res = {}
+    for rule in data:
+        split = re.split(r' bags contain | bags, | bag,', rule)
+        bag_color = split[0]
+        contained_bags = []
+        for contained in split[1:]:
+            stripped = re.sub(r' bags\.| bag\.', '', contained)
+            count = re.search(r'\d+', stripped)
+            if count:
+                color = re.sub(r'\d+', '', stripped).strip()
+                contained_bags.append((count.group(), color))
+        res[bag_color] = contained_bags
+    return res
+
+
+def count_contained_recursive(color, parsed_rules, n=0):
+    n = sum([int(bag[0]) for bag in parsed_rules[color]])
+    for bag in parsed_rules[color]:
+        n += count_contained_recursive(bag[1], parsed_rules) * int(bag[0])
+    return n
+
+
+parsed_rules = parse_rules(input)
+print(count_contained_recursive("shiny gold", parsed_rules))
