0275c3d81827e0d00921d9579482f5d597982be2
francois07 <francois.soulie@edu.esiee.fr>
2020-12-11 22:48:34 +0100

    day11

--- day11/d11p1.py
+++ day11/d11p1.py
@@ -1,60 +1,50 @@
 input = [list(x) for x in open('input.txt', 'r').read().splitlines()]
 
 
-def find_adjacent(data, x, y):
-    coords = [(y-1, x), (y, x+1), (y+1, x), (y, x-1),
-              (y-1, x+1), (y-1, x-1), (y+1, x-1), (y+1, x+1)]
-    res = [data[c[0]][c[1]] for c in coords if (
-        0 <= c[0] < len(data)) and (0 <= c[1] < len(data[0]))]
+def count_adjacent_occupied(data, x, y):
+    coords = [(-1, 0), (0, 1), (1, 0), (0, -1),
+              (-1, 1), (-1, -1), (1, -1), (1, 1)]
+    res = [data[y+a][x+b] for a, b in coords if (
+        0 <= y+a < len(data)) and (0 <= x+b < len(data[0]))].count("#")
     return res
 
 
-def empty_rule(data):
-    res = []
+def rule_coords(data):
+    res = {
+        "L": [],
+        "#": []
+    }
     for i, row in enumerate(data):
         for j, seat in enumerate(row):
             if seat == 'L':
-                if find_adjacent(data, j, i).count("#") == 0:
-                    res.append((i, j))
-    return res
-
-
-def occupied_rule(data):
-    res = []
-    for i, row in enumerate(data):
-        for j, seat in enumerate(row):
-            if seat == '#':
-                if find_adjacent(data, j, i).count("#") >= 4:
-                    res.append((i, j))
+                if count_adjacent_occupied(data, j, i) == 0:
+                    res["#"].append((i, j))
+            if seat == "#":
+                if count_adjacent_occupied(data, j, i) >= 4:
+                    res["L"].append((i, j))
     return res
 
 
 def apply_rules(data):
     new_grid = [row[:] for row in data]
-    new_occupied = empty_rule(new_grid)
-    new_empty = occupied_rule(new_grid)
+    coords = rule_coords(new_grid)
 
-    for coord in new_occupied:
+    for coord in coords["#"]:
         new_grid[coord[0]][coord[1]] = "#"
-    for coord in new_empty:
+    for coord in coords["L"]:
         new_grid[coord[0]][coord[1]] = "L"
 
     return new_grid
 
 
 def until_no_change(data):
-    tmp = [row[:] for row in data]
-    applied = apply_rules(tmp)
-    i = 1
-    while applied != tmp:
-        tmp = applied
-        applied = apply_rules(tmp)
-        if applied == tmp:
-            break
+    grid = [row[:] for row in data]
+    i = 0
+    while apply_rules(grid) != grid:
+        grid = apply_rules(grid)
         i += 1
-    return (i, applied)
+    return (i, sum([row.count("#") for row in grid]))
 
 
 n = until_no_change(input)
-print(n[0])
-print(sum([row.count("#") for row in n[1]]))
+print(n)
--- day11/d11p2.py
+++ day11/d11p2.py
@@ -1,70 +1,64 @@
-import numpy as np
-
 input = [list(x) for x in open('input.txt', 'r').read().splitlines()]
 
 
-def count_adjacent_occupied(data, x, y):
-    res = 0
-    found = []
+def occupied_adjacent(data, x, y, direction):
     for i in range(1, len(data)):
-        coords = [(y-i, x), (y, x+i), (y+i, x), (y, x-i),
-                  (y-i, x+i), (y-i, x-i), (y+i, x-i), (y+i, x+i)]
-        step = []
-        for i, c in enumerate(coords):
-            if (0 <= c[0] < len(data)) and (0 <= c[1] < len(data[0])) and i not in found:
-                if data[c[0]][c[1]] == "#":
-                    found.append(i)
-                step.append(data[c[0]][c[1]])
-        res += step.count("#")
+        dx = x+direction[1]*i
+        dy = y+direction[0]*i
+        if 0 <= dy < len(data) and 0 <= dx < len(data[0]):
+            seat = data[dy][dx]
+            if seat == "#":
+                return True
+            elif seat == "L":
+                return False
+        else:
+            return False
+
+
+def count_adjacent_occupied(data, x, y):
+    coords = [(-1, 0), (0, 1), (1, 0), (0, -1),
+              (-1, 1), (-1, -1), (1, -1), (1, 1)]
+    res = [occupied_adjacent(data, x, y, (a, b))
+           for a, b in coords].count(True)
     return res
 
 
-def empty_rule(data):
-    res = []
+def rule_coords(data):
+    res = {
+        "L": [],
+        "#": []
+    }
     for i, row in enumerate(data):
         for j, seat in enumerate(row):
             if seat == 'L':
                 if count_adjacent_occupied(data, j, i) == 0:
-                    res.append((i, j))
-    return res
-
-
-def occupied_rule(data):
-    res = []
-    for i, row in enumerate(data):
-        for j, seat in enumerate(row):
-            if seat == '#':
+                    res["#"].append((i, j))
+            if seat == "#":
                 if count_adjacent_occupied(data, j, i) >= 5:
-                    res.append((i, j))
+                    res["L"].append((i, j))
     return res
 
 
 def apply_rules(data):
     new_grid = [row[:] for row in data]
-    new_occupied = empty_rule(new_grid)
-    new_empty = occupied_rule(new_grid)
+    coords = rule_coords(new_grid)
 
-    for coord in new_occupied:
+    for coord in coords["#"]:
         new_grid[coord[0]][coord[1]] = "#"
-    for coord in new_empty:
+    for coord in coords["L"]:
         new_grid[coord[0]][coord[1]] = "L"
 
     return new_grid
 
 
 def until_no_change(data):
-    tmp = [row[:] for row in data]
-    applied = apply_rules(tmp)
-    i = 1
-    while applied != tmp:
-        tmp = applied
-        applied = apply_rules(tmp)
-        if applied == tmp:
-            break
+    grid = [row[:] for row in data]
+    i = 0
+    while apply_rules(grid) != grid:
+        grid = apply_rules(grid)
         i += 1
-    return (i, applied)
+    return (i, sum([row.count("#") for row in grid]))
 
 
 n = until_no_change(input)
-print(n[0])
-print(sum([row.count("#") for row in n[1]]))
+print(n)
