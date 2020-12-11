4ca5bbd0d971c78d894197009304f539e400131a
francois07 <francois.soulie@edu.esiee.fr>
2020-12-11 12:16:56 +0100

    fixed logical error

--- day1/d1p1.py
+++ day1/d1p1.py
@@ -3,9 +3,9 @@ input = [int(x) for x in open('input.txt', 'r').read().splitlines()]
 
 def find_pair(data, sum=2020):
     res = -1
-    for i in input:
-        for j in input:
-            if i+j == 2020:
+    for i in data:
+        for j in data:
+            if i+j == sum:
                 res = i*j
     return res
 
--- day1/d1p2.py
+++ day1/d1p2.py
@@ -3,10 +3,10 @@ input = [int(x) for x in open('input.txt', 'r').read().splitlines()]
 
 def find_triplet(data, sum=2020):
     res = -1
-    for i in input:
-        for j in input:
-            for h in input:
-                if i+j+h == 2020:
+    for i in data:
+        for j in data:
+            for h in data:
+                if i+j+h == sum:
                     res = i*j*h
     return res
 
